ce240dd9609ea7f63acd4ec105a424e3d2a83d0d
Dane Ahern <danielaherniv@gmail.com>
2019-10-28 19:52:55 -0400

    set up model folder

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    rquired: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Data,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
