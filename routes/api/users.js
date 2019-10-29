6784ad3bed42b42118234690519f59098ab25883
Dane Ahern <danielaherniv@gmail.com>
2019-10-28 20:15:33 -0400

    added auth.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 // @route  Post api/users
 // @desc   Register user
 // @access Public
-router.post('/', (req, res) => res.send('User route'));
-
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('User route');
+});
 module.exports = router;
