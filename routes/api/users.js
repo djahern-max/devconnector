125727beb5db0dce62868b2210995d4a0fb7a9b9
Dane Ahern <danielaherniv@gmail.com>
2019-10-28 23:32:28 -0400

    working on user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
@@ -31,16 +35,43 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        res.status(400).json({ erors: [{ msg: 'User alread exists' }] });
+        return res.status(400).json({ erors: [{ msg: 'User alread exists' }] });
       }
 
-      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-      // Encrypt password
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-      // Return jsonwebtoken
+      const salt = await bcrypt.genSalt(10);
 
-      res.send('User route');
+      user.password = await bcrypt.hash(password, salt);
+
+      user.save();
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
