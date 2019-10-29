c5ec4d4843fb916031ce76b2791b0a1c4935b210
Dane Ahern <danielaherniv@gmail.com>
2019-10-28 22:02:56 -0400

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,11 +2,49 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route  Post api/users
 // @desc   Register user
 // @access Public
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('User route');
-});
+router.post(
+  '/',
+  [
+    check('name', 'name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please ennter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ erors: [{ msg: 'User alread exists' }] });
+      }
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jsonwebtoken
+
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 module.exports = router;
