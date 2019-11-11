a496cbc58b841712bfd15a634b34e006d1d2b20c
Dano <danielaherniv@gmail.com>
2019-11-10 22:25:39 -0500

    dashboard should be empty

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,12 +1,11 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-// import axios from 'axios';
-
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -15,40 +14,29 @@ const Register = ({ setAlert }) => {
   });
 
   const { name, email, password, password2 } = formData;
+
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
+
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger');
     } else {
-      // const newUser = {
-      //     name,
-      //     email,
-      //     password
-      // }
-      // try {
-      //     const config = {
-      //         headers: {
-      //         'Content-Type': 'application/json'
-      //         }
-      //     }
-      //     const body = JSON.stringify(newUser);
-      //     const res = await axios.post('/api/users', body, config);
-      //     console.log(res.data);
-      // } catch (err) {
-      //     console.error(err.response.data);
-      // }
+      register({ name, email, password });
     }
   };
+
+  if (isAuthenticated) {
+    return <Redirect to='/dashboard' />;
+  }
 
   return (
     <Fragment>
       <h1 className='large text-primary'>Sign Up</h1>
       <p className='lead'>
-        <i className='fas fa-user'></i> Create Your Account
+        <i className='fas fa-user' /> Create Your Account
       </p>
-
       <form className='form' onSubmit={e => onSubmit(e)}>
         <div className='form-group'>
           <input
@@ -57,10 +45,8 @@ const Register = ({ setAlert }) => {
             name='name'
             value={name}
             onChange={e => onChange(e)}
-            required
           />
         </div>
-
         <div className='form-group'>
           <input
             type='email'
@@ -68,7 +54,6 @@ const Register = ({ setAlert }) => {
             name='email'
             value={email}
             onChange={e => onChange(e)}
-            required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -82,10 +67,8 @@ const Register = ({ setAlert }) => {
             name='password'
             value={password}
             onChange={e => onChange(e)}
-            minLength='6'
           />
         </div>
-
         <div className='form-group'>
           <input
             type='password'
@@ -93,25 +76,28 @@ const Register = ({ setAlert }) => {
             name='password2'
             value={password2}
             onChange={e => onChange(e)}
-            minLength='6'
           />
         </div>
-
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
-
       <p className='my-1'>
-        Already have an account? <Link to='login'>Sign In</Link>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
 
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
 export default connect(
-  null,
-  { setAlert }
+  mapStateToProps,
+  { setAlert, register }
 )(Register);
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
+export const Navbar = ({ auth: { isAuthenticated, loading } }) => {
+  const authLinks = (
+    <i>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt'> </i> Logout
+        </a>
+      </li>
+    </i>
+  );
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='!#'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code'></i> DevConnector
+        </Link>
+      </h1>
+    </nav>
+  );
+};
 
-export const Navbar = () => {
-    return (
-        <nav className="navbar bg-dark">
-            <h1>
-                <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
-            </h1>
-            <ul>
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
-                <li><a href="!#">Developers</a></li>
-                <li>
-                    <Link to="/register">Register</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-            </ul>
-        </nav>
-    )
-}
-
-export default Navbar
+export default connect(
+  mapStateToProps,
+  { logout }
+)(Navbar);
