002f010b2945fe0055320585fc95049e3ea3c0cb
Dano <danielaherniv@gmail.com>
2019-11-27 06:13:21 -0500

    update

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,32 +9,51 @@ const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const Post = require('../../models/Post');
 
-// @route  GET api/profile/me
-// @desc   Get curren tusers profile
-// @access Private
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
 router.get('/me', auth, async (req, res) => {
   try {
-    const profile = await Profile.findOne({
-      user: req.user.id
-    }).populate('user', ['name', 'avatar']);
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
 
     if (!profile) {
-      return res.status(400).json({
-        msg: 'There is no profile for this user'
-      });
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
 
     res.json(profile);
   } catch (err) {
-    console.error(err, message);
+    console.error(err.message);
     res.status(500).send('Server Error');
   }
 });
 
-// @route  POST api/profile/me
-// @desc   Create or update user profile
-// @access Private
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
 
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST api/profile
+// @desc     Create or update user profile
+// @access   Private
 router.post(
   '/',
   [
@@ -51,9 +70,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({
-        errors: errors.array()
-      });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const {
@@ -71,7 +88,7 @@ router.post(
       linkedin
     } = req.body;
 
-    //Build profile object
+    // Build profile object
     const profileFields = {};
     profileFields.user = req.user.id;
     if (company) profileFields.company = company;
@@ -84,7 +101,7 @@ router.post(
       profileFields.skills = skills.split(',').map(skill => skill.trim());
     }
 
-    //Build social object
+    // Build social object
     profileFields.social = {};
     if (youtube) profileFields.social.youtube = youtube;
     if (twitter) profileFields.social.twitter = twitter;
@@ -93,23 +110,12 @@ router.post(
     if (instagram) profileFields.social.instagram = instagram;
 
     try {
+      // Using upsert option (creates new doc if no match is found):
       let profile = await Profile.findOneAndUpdate(
-        {
-          user: req.user.id
-        },
-        {
-          $set: profileFields
-        },
-        {
-          new: true,
-          upsert: true
-        }
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, upsert: true }
       );
-      res.json(profile);
-
-      profile = new Profile(profileFields);
-
-      await profile.save();
       res.json(profile);
     } catch (err) {
       console.error(err.message);
@@ -118,10 +124,22 @@ router.post(
   }
 );
 
-// @route  GET api/profile
-// @desc   Get profile by user ID
-// @access Public
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
 router.get('/user/:user_id', async (req, res) => {
   try {
     const profile = await Profile.findOne({
