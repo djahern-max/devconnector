d0d70998f41a85ad3a65a8a35b787490d1e0110f
Dano <danielaherniv@gmail.com>
2019-11-28 08:35:12 -0500

    finished app

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,7 +2,8 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import PostItem from '../posts/PostItem';
+import PostItem from './PostItem';
+import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
@@ -10,22 +11,22 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
         getPosts();
     }, [getPosts]);
 
-    return loading ?
-        <Spinner /> :
-        <Fragment>
-            <h1 className='large text-primary'>Posts</h1>
-            <p className='lead'>
-                <i className='fas fa-user' /> Welcome to the community
+    return loading ? (
+        <Spinner />
+    ) : (
+            <Fragment>
+                <h1 className='large text-primary'>Posts</h1>
+                <p className='lead'>
+                    <i className='fas fa-user' /> Welcome to the community
       </p>
-            {/*<PostForm />*/}
-            <div className='posts'>
-                {posts.map(post => (
-                    <PostItem key={post._id} post={post} />
-                ))}
-
-            </div>
-        </Fragment>
-
+                <PostForm />
+                <div className='posts'>
+                    {posts.map(post => (
+                        <PostItem key={post._id} post={post} />
+                    ))}
+                </div>
+            </Fragment>
+        );
 };
 
 Posts.propTypes = {
@@ -41,3 +42,4 @@ export default connect(
     mapStateToProps,
     { getPosts }
 )(Posts);
+
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,7 +1,13 @@
 import {
     GET_POSTS,
     POST_ERROR,
-    UPDATE_LIKES
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST,
+    GET_POST,
+    ADD_COMMENT,
+    REMOVE_COMMENT
+
 } from '../actions/types';
 
 const initialState = {
@@ -20,6 +26,24 @@ export default function (state = initialState, action) {
                 ...state,
                 posts: payload,
                 loading: false
+            };
+        case GET_POST:
+            return {
+                ...state,
+                posts: payload,
+                loading: false
+            };
+        case ADD_POST:
+            return {
+                ...state,
+                posts: [payload, ...state.posts,],
+                loading: false
+            }
+        case DELETE_POST:
+            return {
+                ...state,
+                posts: state.posts.filter(post => post._id !== payload),
+                loading: false
             }
         case POST_ERROR:
             return {
@@ -35,6 +59,23 @@ export default function (state = initialState, action) {
                 ),
                 loading: false
             };
+        case ADD_COMMENT:
+            return {
+                ...state,
+                post: { ...state.post, comments: payload },
+                loading: false
+            }
+        case REMOVE_COMMENT:
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id !== payload),
+                    loading: false
+                }
+            }
+
+
         default:
             return state;
     }
